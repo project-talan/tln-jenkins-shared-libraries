74ea714074c21665004da9862e4dfdda45989150
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2020-05-15 12:42:57 +0000

    rebane/restructure parameters

--- vars/componentParams.groovy
+++ vars/componentParams.groovy
@@ -1,16 +1,18 @@
 def getCommonParameters(mask, defaults = null) {
   def result = []
   def params = [
-    ['type': 'string',  'name': 'COMPONENT_PARAM_HOST',    'flag': paramConstant.COMPONENT_PARAM_HOST,     'default': 'localhost' ],
-    ['type': 'string',  'name': 'COMPONENT_PARAM_LSTN',    'flag': paramConstant.COMPONENT_PARAM_LSTN,     'default': '0.0.0.0' ],
-    ['type': 'string',  'name': 'COMPONENT_PARAM_PORT',    'flag': paramConstant.COMPONENT_PARAM_PORT,     'default': '9080' ],
-    ['type': 'string',  'name': 'COMPONENT_PARAM_PORTS',   'flag': paramConstant.COMPONENT_PARAM_PORTS,    'default': '9443' ],
-    ['type': 'string',  'name': 'TALAN_PRESETS_PATH',      'flag': paramConstant.TALAN_PRESETS_PATH,       'default': '/tmp' ],
-    ['type': 'string',  'name': 'SONARQUBE_SERVER',        'flag': paramConstant.SONARQUBE_SERVER,         'default': '' ],
-    ['type': 'string',  'name': 'SONARQUBE_SCANNER',       'flag': paramConstant.SONARQUBE_SCANNER,        'default': '' ],
-    ['type': 'boolean', 'name': 'SONARQUBE_QUALITY_GATES', 'flag': paramConstant.SONARQUBE_QUALITY_GATES,  'default': '' ],
-    ['type': 'string',  'name': 'SONARQUBE_ACCESS_TOKEN',  'flag': paramConstant.SONARQUBE_ACCESS_TOKEN,   'default': '' ],
-    ['type': 'string',  'name': 'GITHUB_ACCESS_TOKEN',     'flag': paramConstant.GITHUB_ACCESS_TOKEN,      'default': '' ]
+    ['type': 'string',  'name': 'TLN_COMPONENT_PARAM_HOST',   'flag': paramConstant.TLN_COMPONENT_PARAM_HOST,     'default': 'localhost' ],
+    ['type': 'string',  'name': 'TLN_COMPONENT_PARAM_LSTN',   'flag': paramConstant.TLN_COMPONENT_PARAM_LSTN,     'default': '0.0.0.0' ],
+    ['type': 'string',  'name': 'TLN_COMPONENT_PARAM_PORT',   'flag': paramConstant.TLN_COMPONENT_PARAM_PORT,     'default': '9080' ],
+    ['type': 'string',  'name': 'TLN_COMPONENT_PARAM_PORTS',  'flag': paramConstant.TLN_COMPONENT_PARAM_PORTS,    'default': '9443' ],
+    ['type': 'string',  'name': 'TLN_SHARED_COMPONENTS_HOME', 'flag': paramConstant.TLN_SHARED_COMPONENTS_HOME,   'default': '/tmp/tln' ],
+
+    ['type': 'string',  'name': 'TLN_GITHUB_ACCESS_TOKEN',    'flag': paramConstant.TLN_GITHUB_ACCESS_TOKEN,      'default': '' ],
+
+    ['type': 'string',  'name': 'TLN_SONARQUBE_SERVER',        'flag': paramConstant.SONARQUBE_SERVER,            'default': '' ],
+    ['type': 'string',  'name': 'TLN_SONARQUBE_SCANNER',       'flag': paramConstant.SONARQUBE_SCANNER,           'default': '' ],
+    ['type': 'boolean', 'name': 'TLN_SONARQUBE_QUALITY_GATES', 'flag': paramConstant.SONARQUBE_QUALITY_GATES,     'default': '' ],
+    ['type': 'string',  'name': 'TLN_SONARQUBE_ACCESS_TOKEN',  'flag': paramConstant.SONARQUBE_ACCESS_TOKEN,      'default': '' ]
   ]
   params.each {
     if ((mask & it.flag) == it.flag) {
--- vars/paramConstant.groovy
+++ vars/paramConstant.groovy
@@ -1,13 +1,18 @@
 class paramConstant {
-  public static final int COMPONENT_PARAM_HOST    = 0x00000001;
-  public static final int COMPONENT_PARAM_LSTN    = 0x00000002;
-  public static final int COMPONENT_PARAM_PORT    = 0x00000004;
-  public static final int COMPONENT_PARAM_PORTS   = 0x00000008;
-  public static final int TALAN_PRESETS_PATH      = 0x00000010;
-  public static final int SONARQUBE_SERVER        = 0x00000020;
-  public static final int SONARQUBE_SCANNER       = 0x00000040;
-  public static final int SONARQUBE_QUALITY_GATES = 0x00000080;
-  public static final int SONARQUBE_ACCESS_TOKEN  = 0x00000100;
-  public static final int GITHUB_ACCESS_TOKEN     = 0x00000200;
-  public static final int PARAMS_COMMON           = COMPONENT_PARAM_HOST | COMPONENT_PARAM_LSTN | COMPONENT_PARAM_PORT | COMPONENT_PARAM_PORTS | TALAN_PRESETS_PATH | SONARQUBE_SERVER | SONARQUBE_SCANNER | SONARQUBE_QUALITY_GATES | SONARQUBE_ACCESS_TOKEN | GITHUB_ACCESS_TOKEN;
+  public static final int TLN_COMPONENT_PARAM_HOST      = 0x00000001;
+  public static final int TLN_COMPONENT_PARAM_LSTN      = 0x00000002;
+  public static final int TLN_COMPONENT_PARAM_PORT      = 0x00000004;
+  public static final int TLN_COMPONENT_PARAM_PORTS     = 0x00000008;
+  public static final int TLN_SHARED_COMPONENTS_HOME    = 0x00000010;
+  public static final int TLN_PARAMS_COMMON             = TLN_COMPONENT_PARAM_HOST | TLN_COMPONENT_PARAM_LSTN | TLN_COMPONENT_PARAM_PORT | TLN_COMPONENT_PARAM_PORTS | TLN_SHARED_COMPONENTS_HOME;
+
+  public static final int TLN_GITHUB_ACCESS_TOKEN       = 0x00000100;
+  public static final int TLN_PARAMS_SCM                = TLN_GITHUB_ACCESS_TOKEN;
+
+
+  public static final int TLN_SONARQUBE_SERVER          = 0x00001000;
+  public static final int TLN_SONARQUBE_SCANNER         = 0x00002000;
+  public static final int TLN_SONARQUBE_QUALITY_GATES   = 0x00004000;
+  public static final int TLN_SONARQUBE_ACCESS_TOKEN    = 0x00008000;
+  public static final int TLN_PARAMS_SONARQUBE          = TLN_SONARQUBE_SERVER | TLN_SONARQUBE_SCANNER | TLN_SONARQUBE_QUALITY_GATES | TLN_SONARQUBE_ACCESS_TOKEN;
 }
